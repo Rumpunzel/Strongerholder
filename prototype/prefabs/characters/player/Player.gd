8863754417f89614eaed245e395556119b29449d
Rumpunzel <bencharki.jamal@gmail.com>
2020-03-16 09:39:49 +0100

    added controller support and simplified movement

--- prototype/prefabs/characters/player/Player.gd
+++ prototype/prefabs/characters/player/Player.gd
@@ -18,25 +18,11 @@ func _ready():
 
 
 func get_radius_change(delta:float):
-	var vertical_velocity = 0
-	
-	if Input.is_action_pressed("ui_up"):
-		vertical_velocity -= walkspeed * up_down_modifier
-	elif Input.is_action_pressed("ui_down"):
-		vertical_velocity += walkspeed * up_down_modifier
-	else:
-		vertical_velocity = 0
+	var vertical_velocity = (Input.get_action_strength("move_down") - Input.get_action_strength("move_up")) * walkspeed * up_down_modifier
 	
 	ring_radius += vertical_velocity * delta
 
 func get_position_change(delta:float):
-	var velocity = 0
-	
-	if Input.is_action_pressed("ui_left"):
-		velocity -= walkspeed
-	elif Input.is_action_pressed("ui_right"):
-		velocity += walkspeed
-	else:
-		velocity = 0
+	var velocity = (Input.get_action_strength("move_right") - Input.get_action_strength("move_left")) * walkspeed
 	
 	ring_position += (velocity / (ring_radius * 2 * PI)) * delta
