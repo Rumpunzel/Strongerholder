7fa30fa573bf14b32fd4bd70f96a555a1e6477c1
Rumpunzel <bencharki.jamal@gmail.com>
2020-03-17 15:04:25 +0100

    simplified sprite orientation

--- prototype/prefabs/characters/BASE/sprite.gd
+++ prototype/prefabs/characters/BASE/sprite.gd
@@ -13,8 +13,4 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if not camera == null:
-		var camera_pos = camera.global_transform.origin
-		camera_pos.y = global_transform.origin.y
-		look_at(camera_pos, Vector3.UP)
-		
-		flip_h = global_transform.origin.distance_to(camera.global_transform.origin) < camera.global_transform.origin.distance_to(Vector3())
+		flip_h = global_transform.origin.distance_to(camera.global_transform.origin) > camera.global_transform.origin.distance_to(Vector3())
