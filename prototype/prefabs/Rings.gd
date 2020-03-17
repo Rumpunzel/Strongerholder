d96973f37d5b15603639232558b57237f80f0b29
Rumpunzel <bencharki.jamal@gmail.com>
2020-03-17 19:55:04 +0100

    made building offset absolute again and implemented new and improved ring handling

--- prototype/prefabs/Rings.gd
+++ prototype/prefabs/Rings.gd
@@ -3,7 +3,7 @@ extends Spatial
 class_name Rings
 
 
-const BUILDING_OFFSET:float = 0.8
+const BUILDING_OFFSET:float = 2.0
 const BUILDING_SLOP:float = 2.0
 
 
@@ -27,15 +27,12 @@ func construct_rings():
 		build_ring(i)
 
 func build_ring(ring_number):
-	var side_length = GameConstants.get_side_length(ring_number)
-	# HACK
-	var buildings_per_side:int = side_length / (GameConstants.SEGMENT_WIDTH * 3)
-	var number_of_buildings:int = 4 * buildings_per_side
+	var number_of_buildings:int = GameConstants.get_number_of_segments(ring_number)
 	print("total buildings for ring %d: %d" % [ring_number, number_of_buildings])
 	
 	for i in range(number_of_buildings):
 		var new_building = building_fundament.instance()
 		add_child(new_building)
 		#-sqrt(ring_number * 40)
-		new_building.offset(Vector3(0, -pow(ring_number, 2), GameConstants.get_radius_minimum(ring_number) * BUILDING_OFFSET))
+		new_building.offset(Vector3(0, -pow(ring_number, 2), GameConstants.get_radius_minimum(ring_number) - BUILDING_OFFSET))
 		new_building.rotation.y = i * (TAU * (1.0 / number_of_buildings))
