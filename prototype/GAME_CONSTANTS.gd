ec484a86bf193b4e25938d546fc5eca54b00e119
Rumpunzel <bencharki.jamal@gmail.com>
2020-03-19 12:03:19 +0100

    implemented A* search to navigate through the grid

--- prototype/GAME_CONSTANTS.gd
+++ prototype/GAME_CONSTANTS.gd
@@ -8,12 +8,25 @@ const RING_GAP:float = 0.7
 
 const SEGMENT_WIDTH:float = 12.0
 
+const EMPTY = "empty"
+const BRIDGES = "bridges"
+const BUILDINGS = "buildings"
+const EVERYTHING = "everything"
+
+
+onready var pathfinder = AStar.new()
+
 
 var radius_minimums:Dictionary = { }
-var ring_widths:Dictionary = { }
 var side_lengths:Dictionary = { }
 
-var blocks:Dictionary = { }
+var segments_dictionary:Dictionary = { }
+var search_dictionary:Dictionary = { }
+
+var adjacency_matrix:Dictionary = { }
+var bridge_distances:Dictionary = { }
+
+var astar_nodes:Dictionary = { }
 
 
 
@@ -55,14 +68,8 @@ func get_radius_minimum(ring:int) -> int:
 
 # The width of a ring in world distance
 #	the world is organized in concetric rings around the centre and traveling from ring to ring is only possible in special cases
-func get_ring_width(ring:int) -> float:
-	var width = ring_widths.get(ring)
-	
-	if width == null:
-		width = get_radius_minimum(ring + 1) - get_radius_minimum(ring)
-		ring_widths[ring] = width
-	
-	return width
+func get_ring_width() -> float:
+	return GROWTH_FACTOR * SEGMENT_WIDTH
 
 
 func get_number_of_segments(ring:int) -> int:
@@ -74,3 +81,127 @@ func get_segment(ring_position:float, ring_radius:float, without_base_radius:boo
 	var total_segments = get_number_of_segments(current_ring)
 	
 	return int(((ring_position + PI / total_segments) / TAU) * total_segments + total_segments) % total_segments
+
+
+func register_segment(type:String, ring:int, segment:int, object):
+	segments_dictionary[type] = segments_dictionary.get(type, { })
+	segments_dictionary[type][ring] = segments_dictionary[type].get(ring, { })
+	segments_dictionary[type][ring][segment] = object
+
+
+func _construct_pathfinder():
+	var graph_size:int = 0
+	
+	for ring in search_dictionary.keys():
+		for segment in search_dictionary[ring].keys():
+			var building = search_dictionary[ring][segment]
+			
+			pathfinder.add_point(graph_size, building.world_position())
+			astar_nodes[Vector2(ring, segment)] = graph_size
+			
+			graph_size += 1
+	
+	
+	var bridges:Dictionary = segments_dictionary[BRIDGES]
+	var buildings:Dictionary = segments_dictionary[BUILDINGS]
+	
+	for ring in search_dictionary.keys():
+		for segment in search_dictionary[ring].keys():
+			for building in search_dictionary[ring].keys():
+				if abs(segment - building) == 1:
+					pathfinder.connect_points(astar_nodes[Vector2(ring, segment)], astar_nodes[Vector2(ring, building)])
+				
+			for bridge in bridges.get(ring + 1, { }).keys():
+				pathfinder.connect_points(astar_nodes[Vector2(ring, segment)], astar_nodes[Vector2(ring + 1, bridge)])
+	
+	for ring in bridges.keys():
+		for segment in bridges[ring].keys():
+			pass
+	
+	
+#	for ring in bridges.keys():
+#		for segment in bridges[ring].keys():
+#			var start = bridges[ring][segment]
+#
+#			for ring_connection in bridges.keys():
+#				for segment_connection in bridges[ring_connection].keys():
+#					var end = bridges[ring_connection][segment_connection]
+#					var ring_distance = abs(ring_connection - ring)
+#
+#					var segment_distance = abs(segment_connection - segment)
+#					segment_distance = (segment_distance % int(get_number_of_segments(ring) / 2.0)) if segment_distance > get_number_of_segments(ring) / 2.0 else segment_distance
+#
+#					if ring_distance <= 1:
+#						adjacency_matrix[start][end] = segment_distance + ring_distance * (get_ring_width() / SEGMENT_WIDTH)
+
+func _construct_search_dictionary():
+	for type in segments_dictionary.values():
+		for ring in type.keys():
+			for block in type[ring].keys():
+				search_dictionary[ring] = search_dictionary.get(ring, { })
+				search_dictionary[ring][block] = type[ring][block]
+
+func _construct_adjanceny_matrix():
+	var bridges:Dictionary = segments_dictionary[BRIDGES]
+	
+	for ring in bridges.keys():
+		for segment in bridges[ring].keys():
+			var start = Vector2(ring, segment)
+			adjacency_matrix[start] = { }
+			
+			for ring_connection in bridges.keys():
+				for segment_connection in bridges[ring_connection].keys():
+					var end = Vector2(ring_connection, segment_connection)
+					var ring_distance = abs(ring_connection - ring)
+					
+					var segment_distance = abs(segment_connection - segment)
+					segment_distance = (segment_distance % int(get_number_of_segments(ring) / 2.0)) if segment_distance > get_number_of_segments(ring) / 2.0 else segment_distance
+					
+					if ring_distance <= 1:
+						adjacency_matrix[start][end] = segment_distance + ring_distance * (get_ring_width() / SEGMENT_WIDTH)
+	
+	_print_adjacency_matrix()
+
+func _print_adjacency_matrix():
+	var matrix_string:String = ""
+	var header_string:String = "\t"
+	var row_counter = 0
+	
+	
+	for row in adjacency_matrix.keys():
+		var column_counter = 0
+		
+		for column in adjacency_matrix.keys():
+			if row_counter == 0:
+				header_string += "%s\t" % [column]
+			
+			if column_counter == 0:
+				matrix_string += "%s\t" % [row]
+			
+			var distance = adjacency_matrix.get(row, { }).get(column)
+			matrix_string += ("  %2d,\t" % [distance]) if not distance == null else "  --,\t"
+			
+			column_counter += 1
+		
+		row_counter += 1
+		matrix_string += "\n"
+	
+	print("%s\n\n%s" % [header_string, matrix_string])
+
+
+func done_building():
+	_construct_search_dictionary()
+	_construct_pathfinder()
+	_construct_adjanceny_matrix()
+
+
+func get_object_at_position(position:Vector2, from:String = EVERYTHING):
+	var object = null
+	var search_through:Dictionary
+	
+	if not from == EVERYTHING:
+		search_dictionary = segments_dictionary[from]
+	else:
+		search_through = search_dictionary
+	
+	return search_dictionary.get(position.y, { }).get(position.x, null)
