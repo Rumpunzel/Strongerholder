131ad3e3b6b77afd6d3ce2ce3491f2d5b549dedc
Rumpunzel <bencharki.jamal@gmail.com>
2020-03-20 21:24:07 +0100

    adjusted fading buildings to be less noticable

--- prototype/singletons/GameConstants.gd
+++ prototype/singletons/GameConstants.gd
@@ -4,7 +4,7 @@ extends Node
 
 const BASE_RADIUS:float = 12.0
 const GROWTH_FACTOR:float = 3.0
-const RING_GAP:float = 10.0
+const RING_GAP:float = 12.0
 const ROAD_WIDTH:float = 5.0
 
 const SEGMENT_WIDTH:float = 12.0
