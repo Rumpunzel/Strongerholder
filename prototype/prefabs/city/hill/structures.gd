f156ca8b8ad7a48ef066e3ca4c90af8ea461e636
Rumpunzel <bencharki.jamal@gmail.com>
2020-03-21 10:49:27 +0100

    made bridge spawning modular

--- prototype/prefabs/city/hill/structures.gd
+++ prototype/prefabs/city/hill/structures.gd
@@ -23,12 +23,13 @@ func _ready():
 
 func construct_ring(ring_number):
 	var number_of_buildings:int = GameConstants.get_number_of_segments(ring_number)
-	
+	var number_of_bridges:int = biggest_factor(number_of_buildings, int((number_of_buildings - 1) / 2.0))
+	print("bridges on ring %d: %d" % [ring_number, number_of_bridges])
 	for i in range(number_of_buildings):
 		var new_building
 		var type
 		
-		if ring_number > 0 and i % int(number_of_buildings / 4.0) == 0:
+		if ring_number > 0 and i % number_of_bridges == 0:
 			new_building = bridge.instance()
 			
 			type = GameConstants.BRIDGES
@@ -51,3 +52,16 @@ func construct_ring(ring_number):
 		GameConstants.register_segment(type, ring_number, i, new_building)
 	
 	print("total buildings for ring %d: %d" % [ring_number, number_of_buildings])
+
+
+func biggest_factor(number:int, upper_limit:int = -1, lower_limit:int = 2) -> int:
+	var big_fac = number
+	
+	if upper_limit == -1:
+		upper_limit = number
+	
+	for i in range(lower_limit, upper_limit + 1):
+		if number % i == 0:
+			big_fac = i
+	print("the biggest factor for %d is: %d" % [number, big_fac])
+	return big_fac
