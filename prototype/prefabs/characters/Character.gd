8d84491169785ccc042547249765f325287bec55
Rumpunzel <bencharki.jamal@gmail.com>
2020-03-21 16:42:53 +0100

    implemented rudimentary bridge movement limitation

--- prototype/prefabs/characters/Character.gd
+++ prototype/prefabs/characters/Character.gd
@@ -21,7 +21,7 @@ var sprinting:float = 1.0
 var jump_speed:float = 30.0
 
 # If the character is able to move between rings, e.g. when using a bridge
-var can_move_rings:bool = false
+var movement_limit:Array = [ [ ], [ ] ] setget set_movement_limit, get_movement_limit
 #warning-ignore:unused_class_variable
 var current_path:Array = [ ]
 
@@ -43,36 +43,35 @@ func _process(delta):
 
 
 func move(direction:Vector2, delta:float):
-	var radius_minimum = RingMap.get_radius_minimum(current_ring) - RingMap.BASE_RADIUS
-	var ring_width = RingMap.RING_WIDTH - RingMap.RING_GAP
-	
-	# Called with the paramter 0 as the according function needs to be implemented by child classes
-	ring_position += get_position_change(direction).y * delta
-	
-	ring_position = modulo_ring_vector(Vector2(0, ring_position)).y
-	
-	rotation.y = ring_position
-	
-	# Called with the paramter 0 as the according function needs to be implemented by child classes
 	ring_radius += get_position_change(direction).x * delta
 	
+	ring_position += get_position_change(direction).y * delta
+	ring_position = modulo_ring_vector(Vector2(0, ring_position)).y
+	
 	# Limiting of the movement between rings
-	if not can_move_rings and not Engine.editor_hint:
-		ring_radius = clamp(ring_radius, radius_minimum, radius_minimum + ring_width)
+	if not Engine.editor_hint:
+		if not movement_limit[0].empty():
+			ring_radius = clamp(ring_radius, movement_limit[0][0], movement_limit[0][1])
+		
+		if not movement_limit[1].empty():
+			ring_position = clamp(ring_position, movement_limit[1][0], movement_limit[1][1])
 	
 	update_ring_vector()
 	
 	body.ring_radius = ring_radius + RingMap.BASE_RADIUS
+	rotation.y = ring_position
+	
+	
+	var radius_minimum = RingMap.get_radius_minimum(current_ring) - RingMap.BASE_RADIUS
+	var ring_width = RingMap.RING_WIDTH - RingMap.RING_GAP
+	
+	if movement_limit[0].empty() and movement_limit[1].empty() and ring_radius > radius_minimum and ring_radius < radius_minimum + ring_width:
+		update_movement_limit(Vector2(current_ring, current_segment))
 	
 	# The position in the world can be displayed with a Vector2
 	#	with the x-axis being the ring_position and
 	#	with the y-axis being the ring_radius
 	emit_signal("moved", Vector2(ring_position, ring_radius + RingMap.BASE_RADIUS))
-	
-	radius_minimum = RingMap.get_radius_minimum(current_ring) - RingMap.BASE_RADIUS
-	
-	if ring_radius > radius_minimum and ring_radius < radius_minimum + ring_width:
-		can_move_rings = false
 	
 	#print("current_ring: %d" % [current_ring])
 	#print("current_segment: %d" % [current_segment])
@@ -94,6 +93,22 @@ func get_position_change(direction:Vector2) -> Vector2:
 	return Vector2(direction.x * vertical_walkspeed, direction.y * walkspeed / (ring_radius + RingMap.BASE_RADIUS)) * sprinting
 
 
+func update_movement_limit(new_position:Vector2):
+	var radius_minimum = RingMap.get_radius_minimum(int(new_position.x)) - RingMap.BASE_RADIUS
+	var ring_width = RingMap.RING_WIDTH - RingMap.RING_GAP
+
+	movement_limit[0] = [radius_minimum, radius_minimum + ring_width]
+	movement_limit[1] = [ ]
+
+
 func update_current_path(_new_position:Vector2):
 	pass
 
+
+
+func set_movement_limit(new_limit:Array):
+	movement_limit = new_limit
+
+
+func get_movement_limit() -> Array:
+	return movement_limit
--- prototype/prefabs/city/buldings/bridge.gd
+++ prototype/prefabs/city/buldings/bridge.gd
@@ -3,7 +3,7 @@ extends BuildingFundament
 class_name Bridge
 
 
-var characters_on_bridge:Array = [ ]
+#var characters_on_bridge:Array = [ ]
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,25 +12,31 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	for character in characters_on_bridge:
-		character.can_move_rings = true
+#func _process(_delta):
+#	for character in characters_on_bridge:
+#		character.can_move_rings = true
 
 
 
 func entered(body):
 	var object = body.get_parent()
 	
-	if object is Character and not object in characters_on_bridge:
-		characters_on_bridge.append(object)
+	if object is Character:# and not object in characters_on_bridge:
+		#characters_on_bridge.append(object)
+		object.movement_limit[0] = [ ]
+		object.movement_limit[1] = [ring_position - 0.2, ring_position + 0.2]
 	
 	.entered(body)
 
 func exited(body):
 	var object = body.get_parent()
-	
-	if object in characters_on_bridge:
-		characters_on_bridge.erase(object)
+#
+#	if object in characters_on_bridge:
+#		characters_on_bridge.erase(object)
+#
+	if object is Character:
+		object.movement_limit[1] = [ ]
+		object.update_ring_vector()
 	
 	.exited(body)
 
