4a9e88e4409bf8c3e9cb89a624002fbb6a0be1be
Rumpunzel <bencharki.jamal@gmail.com>
2020-03-21 16:42:22 +0100

    small aesthetic changes

--- prototype/prefabs/characters/body.gd
+++ prototype/prefabs/characters/body.gd
@@ -1,7 +1,10 @@
 tool
 extends KinematicBody
 
+
+
 onready var default_gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 
 var ring_radius:float = 0.0 setget set_ring_radius, get_ring_radius
 
@@ -13,6 +16,7 @@ var fall_modifer:float = 1.0
 var grounded:bool = false
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -22,11 +26,15 @@ func _ready():
 func _physics_process(delta):
 	if not Engine.editor_hint:
 		fall_speed += default_gravity * fall_modifer * delta
+		
 		move_and_slide(Vector3(0, jump_speed - fall_speed, 0), Vector3.UP)
+		
 		grounded = is_on_floor()
+		
 		if grounded:
 			fall_speed = 0.0
 			jump_speed = 0.0
+
 
 
 func jump(speed:float = 0.0):
@@ -35,6 +43,7 @@ func jump(speed:float = 0.0):
 	elif grounded:
 		jump_speed = speed
 		fall_modifer = 1.0
+
 
 
 func set_ring_radius(new_radius:float):
