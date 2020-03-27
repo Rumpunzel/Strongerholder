f66d8aa0a5b5755a39a44d30c32d717f20f6287c
Rumpunzel <bencharki.jamal@gmail.com>
2020-03-27 10:06:48 +0100

    changed bridge layout

--- prototype/prefabs/city/buldings/bridge/bridge.gd
+++ prototype/prefabs/city/buldings/bridge/bridge.gd
@@ -5,4 +5,4 @@ class_name Bridge
 
 func set_ring_vector(new_vector:RingVector):
 	.set_ring_vector(new_vector)
-	rotation.x = -CityLayout.get_slope_sinus(new_vector.radius + CityLayout.ROAD_WIDTH * 2)
+	rotation.x = -CityLayout.get_slope_sinus(new_vector.radius + CityLayout.RING_WIDTH)
