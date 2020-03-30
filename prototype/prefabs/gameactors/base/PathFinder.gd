ab41862b08abab75d2f2e49fe0fd912bae676b15
Jamal Bencharki <bencharki.jamal@gmail.com>
2020-03-30 17:48:20 +0200

    fixed a pathfinding bug

--- prototype/prefabs/gameactors/base/PathFinder.gd
+++ prototype/prefabs/gameactors/base/PathFinder.gd
@@ -19,6 +19,8 @@ func get_input() -> Array:
 	var next_path_segment:RingVector = current_segments[0] if not current_segments.empty() else null
 	
 	if next_path_segment:
+		next_path_segment.modulo_ring_vector()
+		
 		movement_vector = Vector2(next_path_segment.radius - start_ring_vector.radius, next_path_segment.rotation - start_ring_vector.rotation)
 		movement_vector.x /= 256
 		
@@ -50,7 +52,6 @@ func update_current_path(new_vector:RingVector):
 	if start_ring_vector and pathfinding_target:
 		current_path = ring_map.city_navigator.get_shortest_path(new_vector, pathfinding_target)
 		
-		
 		for segment in range(1, current_path.size()):
 			var new_segment = RingVector.new(current_path[segment].x, current_path[segment].y, true)
 			
@@ -68,7 +69,6 @@ func set_ring_map(new_ring_map:RingMap):
 
 func set_pathfinding_target(new_target:RingVector):
 	pathfinding_target = new_target
-	
 	update_current_path(start_ring_vector)
 
 
