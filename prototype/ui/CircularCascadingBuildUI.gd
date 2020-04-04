39b0f994f3a0832c6b7a2a16352b8d5dd7024ebc
Todbrot <todbrot22@gmail.com>
2020-04-04 08:15:21 +0200

    Very dirty implementation of radial UI for building menu
    Did someone say magic numbers?

---
+++ prototype/ui/CircularCascadingBuildUI.gd
@@ -0,0 +1,49 @@
+tool
+extends Container
+
+export var button_radius = 100 
+
+
+func _ready():
+	place_buttons()
+
+func place_buttons():
+	var buttons = get_children()
+	
+	if buttons.size() == 0:
+		return
+
+	#Amount to change the angle for each button
+	var angle_offset = (2*PI)/buttons.size() #in degrees
+
+	var angle = 1.5*PI #in radians
+	for btn in buttons:
+		if not "_button_pressed" in btn.get_signal_list():
+			btn.connect("pressed", self, "_button_pressed", [btn])
+		btn.visible = true
+		btn.rect_position = Vector2(button_radius, 0).rotated(angle)
+		btn.rect_rotation = angle * 180 / PI + 90
+		angle += angle_offset
+
+func add_button(btn):
+	add_child(btn)
+	place_buttons()
+
+
+func _button_pressed(button):
+	var children = button.get_children()
+	
+	if children.size() == 0:
+		return
+		
+	#Amount to change the angle for each button
+	var angle_offset = (PI)/(children.size()+1) #in degrees
+	
+	var angle = PI + angle_offset #in radians
+	for btn in children:
+		btn.connect("pressed", self, "_button_pressed", [btn])
+		btn.visible = true
+		btn.rect_position = Vector2(button_radius, 0).rotated(angle)
+		btn.rect_rotation = angle * 180 / PI + 90
+		
+		angle += angle_offset
