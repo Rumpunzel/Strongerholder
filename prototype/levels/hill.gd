8938a23257c4c0642efd8f729a099df0ad453daf
Jamal Bencharki <bencharki.jamal@gmail.com>
2020-04-04 17:03:29 +0200

    made hill main scene

--- prototype/levels/hill.gd
+++ prototype/levels/hill.gd
@@ -1,12 +1,16 @@
-extends Spatial
+extends Navigation
 
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	randomize()
 	
 	var rm = RingMap.new()
-	$hill.setup(rm)
+	
+	$navigation_mesh/city_structures.build_everything(rm)
+	$flora.grow_flora(rm)
+	
+	$navigation_mesh
 	
 	var pl = preload("res://prefabs/gameactors/player/Player.tscn").instance()
 	pl.setup(rm)
--- prototype/prefabs/city/hill.gd
+++
@@ -1,8 +0,0 @@
-tool
-extends Spatial
-
-
-# Called when the node enters the scene tree for the first time.
-func setup(new_ring_map:RingMap):
-	$city_structures.build_everything(new_ring_map)
-	$flora.grow_flora(new_ring_map)
