ab4b5e4775bc6aba9849e7144829d7779d819399
Jamal Bencharki <bencharki.jamal@gmail.com>
2020-04-05 14:40:16 +0200

    added a bit of random visual diversity

--- prototype/prefabs/things/GameTree/sprite.gd
+++ prototype/prefabs/things/GameTree/sprite.gd
@@ -9,3 +9,5 @@ export(Array, Texture) var sprites
 func _ready():
 	texture = sprites[randi() % sprites.size()]
 	offset.y = texture.get_height() / 2.0
+	flip_h = randi() % 5 == 0
+	modulate = Color(0.9 + randf() * 0.1, 0.9 + randf() * 0.1, 0.9 + randf() * 0.1)
