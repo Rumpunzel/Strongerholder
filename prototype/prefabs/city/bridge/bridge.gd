e1f841399c4ece1a277b7cc5e08c53fc1638a637
Jamal Bencharki <bencharki.jamal@gmail.com>
2020-04-05 17:48:58 +0200

    removed useless subclasses

--- prototype/prefabs/city/bridge/bridge.gd
+++
@@ -1,8 +0,0 @@
-extends DoodadBuilding
-class_name Bridge
-
-
-
-func set_ring_vector(new_vector:RingVector):
-	.set_ring_vector(new_vector)
-	rotation.x = -CityLayout.get_slope_sinus(new_vector.ring)
