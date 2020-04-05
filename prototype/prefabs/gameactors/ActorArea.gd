df1d0fb5701fb7522ac93e9f5176a8fae3f7d5d1
Jamal Bencharki <bencharki.jamal@gmail.com>
2020-04-05 19:02:05 +0200

    reorgianized GameActor directory

--- prototype/prefabs/gameactors/ActorArea.gd
+++ prototype/prefabs/gameactors/ActorArea.gd
@@ -5,8 +5,8 @@ class_name ActorArea
 
 func _ready():
 	connect("added_object", self, "check_destination")
-	game_object.connect("new_interest", self, "check_destination")
-	game_object.connect("acquired_target", self, "check_destination")
+	get_game_object().connect("new_interest", self, "check_destination")
+	get_game_object().connect("acquired_target", self, "check_destination")
 
 
 
