8f969ef69cf0c152209b4f87b692fd4030ebfea4
Jamal Bencharki <bencharki.jamal@gmail.com>
2020-04-12 21:42:17 +0200

    encapsulated animations state machine behavior

--- prototype/game_objects/game_actors/animations/animation_tree.gd
+++ prototype/game_objects/game_actors/animations/animation_tree.gd
@@ -1,7 +1,13 @@
+class_name AnimationStateMachine
 extends AnimationTree
 
 
 var blend_positions: Vector2 setget set_blend_positions, get_blend_positions
+
+var next_animation: String = "idle"
+
+
+onready var state_machine: AnimationNodeStateMachinePlayback = get("parameters/playback")
 
 
 
@@ -10,6 +16,27 @@ func _init():
 	set_blend_positions(Vector2(0, 1))
 	active = true
 
+
+func _process(_delta):
+	if not is_idle_animation(next_animation):
+		next_animation = "idle"
+
+
+
+
+func travel(new_animation: String):
+	if is_idle_animation(next_animation):
+		next_animation = new_animation
+		
+		state_machine.travel(new_animation)
+
+
+func is_idle() -> bool:
+	return is_idle_animation(next_animation) and is_idle_animation(get_current_state())
+
+
+func is_idle_animation(animation: String = next_animation) -> bool:
+	return animation.begins_with("idle") or animation == "run"
 
 
 
@@ -26,3 +53,6 @@ func set_blend_positions(new_direction: Vector2):
 
 func get_blend_positions() -> Vector2:
 	return blend_positions
+
+func get_current_state() -> String:
+	return state_machine.get_current_node()
