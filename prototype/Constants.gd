b50435f2909985bd8859bc55b17f6c9e78a9be92
Jamal Bencharki <bencharki.jamal@gmail.com>
2020-04-18 16:09:49 +0200

    reformatted enums

--- prototype/Constants.gd
+++ prototype/Constants.gd
@@ -10,27 +10,28 @@ enum {
 }
 
 enum Actors {
-	NOTHING = 64,
-	EVERYTHING,
+	NOTHING = 0,
+	EVERYTHING = 64,
 	PLAYER,
 	WOODSMAN,
 	CARPENTER,
 }
 
 enum Structures {
-	NOTHING = 256,
-	EVERYTHING,
+	NOTHING = 0,
+	EVERYTHING = 256,
 	BASE,
 	BRIDGE,
 	FOUNDATION,
 	STOCKPILE,
 	WOODCUTTERS_HUT,
-	TREE,
+	
+	TREE = 512,
 }
 
 enum Resources {
-	NOTHING = 1024,
-	EVERYTHING,
+	NOTHING = 0,
+	EVERYTHING = 1024,
 	WOOD,
 	WOOD_PLANKS,
 	STONE,
@@ -42,10 +43,13 @@ static func is_actor(index) -> bool:
 	return Actors.values().has(index)
 
 static func is_structure(index) -> bool:
-	return Structures.values().has(index)
+	return index > Structures.EVERYTHING and index < Structures.TREE and Structures.values().has(index)
+
+static func is_thing(index) -> bool:
+	return index >= Structures.TREE and Structures.values().has(index)
 
 static func is_resource(index) -> bool:
-	return Resources.values().has(index)
+	return index > Resources.EVERYTHING and Resources.values().has(index)
 
 static func is_request(index) -> bool:
 	return index is int and index >= REQUEST
