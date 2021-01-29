5f690fcb46506a6d03d03d99056b823a662ebe93
Jamal Bencharki <bencharki.jamal@gmail.com>
2021-01-29 16:39:00 +0100

    replaced service locator null checks with assertions

--- prototype_2/game_objects/ServiceLocator.gd
+++ prototype_2/game_objects/ServiceLocator.gd
@@ -9,42 +9,32 @@ var quarter_master: Node setget register_as_quarter_master
 
 
 func register_as_navigator(new_node: Navigation2D) -> void:
-	if not new_node:
-		return
-	
+	assert(new_node)
 	navigator = new_node
 
 
 func unregister_as_navigator(new_node: Navigation2D) -> void:
-	if not (new_node and new_node == navigator):
-		return
-	
+	assert(new_node and new_node == navigator)
 	navigator = null
 
 
 
 func register_as_objects_layer(new_node: YSort) -> void:
-	if not new_node:
-		return
-	
+	assert(new_node)
 	objects_layer = new_node
 
 
 func unregister_as_objects_layer(new_node: YSort) -> void:
-	if not (new_node and new_node == objects_layer):
-		return
-	
+	assert(new_node and new_node == objects_layer)
 	objects_layer = null
 
 
 
 func register_as_quarter_master(new_node: Node) -> void:
-	if new_node:
-		quarter_master = new_node
+	assert(new_node)
+	quarter_master = new_node
 
 
 func unregister_as_quarter_master(new_node: Node) -> void:
-	if not (new_node and new_node == quarter_master):
-		return
-	
+	assert(new_node and new_node == quarter_master)
 	quarter_master = null
