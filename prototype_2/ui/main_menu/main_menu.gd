10f510c51ff29e38baece548236b7cf0d36b768c
Jamal Bencharki <bencharki.jamal@gmail.com>
2021-02-04 14:35:01 +0100

    added class_editor to the main menu when running in debug mode

--- prototype_2/ui/main_menu/main_menu.gd
+++ prototype_2/ui/main_menu/main_menu.gd
@@ -2,9 +2,14 @@ class_name MainMenu, "res://class_icons/gui/icon_main_menu.svg"
 extends Menu
 
 
+signal open_class_editor
+
+
 onready var _title: Label = $SplitContainer/CenterContainer/MenuLayout/Title
 onready var _version: Label = $SplitContainer/MarginContainer/Version
 onready var _tween: Tween = $Tween
+
+onready var _class_edtior_button : Button = $SplitContainer/CenterContainer/MenuLayout/MenuButtons/ClassEditor
 
 
 
@@ -12,6 +17,8 @@ onready var _tween: Tween = $Tween
 func _ready() -> void:
 	_title.text = Main.get_game_title()
 	_version.text = Main.get_version()
+	
+	_class_edtior_button.visible = OS.is_debug_build()
 
 
 
@@ -33,6 +40,14 @@ func _load_game() -> void:
 	get_tree().paused = true
 	
 	emit_signal("load_game")
+	hide()
+
+
+func _open_class_editor() -> void:
+	if not visible:
+		return
+	
+	emit_signal("open_class_editor")
 	hide()
 
 
