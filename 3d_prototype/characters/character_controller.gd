ed6ea7f1cf71225282894e0c210c5b901f876343
Jamal Bencharki <bencharki.jamal@gmail.com>
2021-07-02 11:50:13 +0200

    added simple character and player scenes

---
+++ 3d_prototype/characters/character_controller.gd
@@ -0,0 +1,92 @@
+class_name CharacterController
+extends KinematicBody
+
+# warning-ignore-all:unused_class_variable
+export(Resource) var movement_stats
+
+
+var velocity: Vector3
+
+var input_vector: Vector2
+
+var movement_input: Vector3
+var horizontal_movement_vector: Vector2
+var vertical_velocity: float
+
+var destination_input: Vector3
+var destination_point: Vector3
+
+var moving_to_destination: bool
+
+var is_grounded: bool
+
+var is_running: bool
+var target_speed: float
+
+var jump_input: bool
+var attack_input: bool
+
+
+var _camera_system: CameraSystem
+
+var _getting_point_from_mouse: bool = false
+var _previous_speed: float
+
+
+onready var _ground_check: RayCast = $GroundCheck
+
+
+
+func _ready() -> void:
+	_camera_system = ServiceLocator.get_service(CameraSystem) as CameraSystem
+	InputReader.listener = self
+
+
+func _process(_delta: float) -> void:
+	if moving_to_destination:
+		_calculate_target_speed(1.0)
+	else:
+		_calculate_target_speed(input_vector.length())
+		destination_input = transform.origin
+	
+	if _getting_point_from_mouse:
+		_get_point_from_mouse()
+	else:
+		_recalculate_movement()
+
+
+func _physics_process(_delta: float) -> void:
+	velocity = move_and_slide(velocity)
+	is_grounded = _ground_check.is_colliding()
+
+
+
+func get_adjusted_movement() -> Vector3:
+	var ajusted_movement: Vector3
+	var camera: Camera = _camera_system.current_camera
+	
+	if camera:
+		var camera_forward: Vector3 = camera.transform.basis.z
+		camera_forward.y = 0.0
+		var camera_right: Vector3 = camera.transform.basis.x
+		camera_right.y = 0.0
+		
+		ajusted_movement = camera_right.normalized() * input_vector.x + camera_forward.normalized() * input_vector.y
+	else:
+		printerr("No gameplay camera in the scene. Movement orientation will not be correct.")
+		ajusted_movement = Vector3(input_vector.x, 0.0, input_vector.y)
+	
+	return ajusted_movement
+
+
+
+func _calculate_target_speed(new_target_speed: float) -> void:
+	_previous_speed = target_speed
+	new_target_speed = clamp(new_target_speed, 0.0, 1.0) * (1.0 if is_running else movement_stats.walking_modifier)
+	target_speed = new_target_speed
+
+func _recalculate_movement() -> void:
+	movement_input = get_adjusted_movement().normalized() * target_speed
+
+func _get_point_from_mouse() -> void:
+	movement_input = Vector3.ZERO
---
+++ 3d_prototype/characters/character_movement_stats_resource.gd
@@ -0,0 +1,25 @@
+class_name CharacterMovementStatsResource
+extends Resource
+
+# warning-ignore-all:unused_class_variable
+export var move_speed: float = 12.0
+export var move_acceleration: float = 12.0
+
+export(float, 0.0, 1.0, 0.1) var walking_modifier: float = 0.7
+
+export var jump_height: float = 1.0
+export var aerial_modifier: float = 1.1
+export var aerial_acceleration: float = 200.0
+
+export var gravity_descend_multilpier: float = 4.0
+export var gravity_ascend_multiplier: float = 2.0
+
+export var air_resistance: float = 5.0
+export var turn_rate: float = 500.0
+
+export var _max_fall_speed: float = 50.0
+export var _max_rise_speed: float = 100.0
+
+
+func validate_vertical_speed(vertical_velocity: float) -> float:
+	return clamp(vertical_velocity, -_max_fall_speed, _max_rise_speed)
