50c4501b5c475206ad0b45160176a707242034ed
Jamal Bencharki <bencharki.jamal@gmail.com>
2021-07-06 15:00:05 +0200

    overhauled gui a tiny bit

---
+++ 3d_prototype/ui/main_menu/main_menu_button.gd
@@ -0,0 +1,50 @@
+extends Button
+tool
+
+onready var _label: Label = $Label
+
+
+func _ready() -> void:
+	# warning-ignore-all:return_value_discarded
+	connect("focus_entered", self, "_set_hover")
+	connect("focus_exited", self, "_set_default")
+	connect("mouse_entered", self, "_set_hover")
+	connect("mouse_exited", self, "_set_default")
+	connect("button_down", self, "_set_pressed")
+	connect("button_up", self, "_set_default")
+	
+	set_text(text)
+	set_disabled(disabled)
+
+
+func set_text(new_text: String) -> void:
+	_label.text = new_text
+
+func get_text() -> String:
+	return _label.text
+
+func set_disabled(is_disabled: bool) -> void:
+	.set_disabled(is_disabled)
+	if disabled:
+		_set_disabled()
+	else:
+		_set_default()
+
+
+func _set_default() -> void:
+	if disabled:
+		return
+	_label.add_color_override("font_color", get_color("font_color"))
+
+func _set_hover() -> void:
+	if disabled:
+		return
+	_label.add_color_override("font_color", get_color("font_color_hover"))
+
+func _set_pressed() -> void:
+	if disabled:
+		return
+	_label.add_color_override("font_color", get_color("font_color_pressed"))
+
+func _set_disabled() -> void:
+	_label.add_color_override("font_color", get_color("font_color_disabled"))
