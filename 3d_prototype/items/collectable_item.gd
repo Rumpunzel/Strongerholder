07c0f7af4fe3257dd067a2e0f24a6af3fd5e50b9
Jamal Bencharki <bencharki.jamal@gmail.com>
2021-07-07 08:11:32 +0200

    created collectable item base scene

--- 3d_prototype/items/collectable_item.gd
+++ 3d_prototype/items/collectable_item.gd
@@ -1,5 +1,16 @@
 class_name CollectableItem
 extends RigidBody
 
-# warning-ignore-all:unused_class_variable
 export(Resource) var item_resource
+
+
+func _get_configuration_warning() -> String:
+	var warning := ""
+	
+	# Data
+	if not item_resource:
+		warning = "ItemResource is required"
+	elif not item_resource is ItemResource:
+		warning = "ItemResource is of the wrong type"
+	
+	return warning
--- 3d_prototype/items/tool_resource.gd
+++ 3d_prototype/items/tool_resource.gd
@@ -2,5 +2,5 @@ class_name ToolResource
 extends ItemResource
 
 # warning-ignore-all:unused_class_variable
-export(Array, Resource) var used_on = [ ]
+export(Array, String) var used_on = [ ]
 export var damage: float = 1.0
