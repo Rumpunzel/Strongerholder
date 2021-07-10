718b2ead526fd1b5ce6ec9a1fa1bb46b0eacfb96
Jamal Bencharki <bencharki.jamal@gmail.com>
2021-07-10 14:23:32 +0200

    merged inventory input handling into one action

--- 3d_prototype/characters/controllers/input_reader/actions/read_inventory_action_resource.gd
+++ 3d_prototype/characters/controllers/input_reader/actions/read_inventory_action_resource.gd
@@ -16,6 +16,11 @@ class ReadInventoryAction extends StateAction:
 		assert(error == OK)
 		error = _inventory.connect("item_removed", self, "_on_item_changed")
 		assert(error == OK)
+		
+		error = _inventory.connect("equipment_added", self, "_on_equipment_changed")
+		assert(error == OK)
+		error = _inventory.connect("equipment_removed", self, "_on_equipment_changed")
+		assert(error == OK)
 	
 	
 	func on_state_enter() -> void:
@@ -24,7 +29,13 @@ class ReadInventoryAction extends StateAction:
 	func on_update(_delta: float) -> void:
 		if Input.is_action_just_released("open_inventory"):
 			Events.hud.emit_signal("inventory_hud_toggled")
+		
+		if Input.is_action_just_pressed("open_equipment_menu"):
+			Events.hud.emit_signal("equipment_hud_toggled")
 	
 	
 	func _on_item_changed(_item: ItemResource = null) -> void:
 		Events.hud.emit_signal("inventory_updated", _inventory)
+	
+	func _on_equipment_changed(_item: ItemResource = null) -> void:
+		Events.hud.emit_signal("equipment_updated", _inventory)
