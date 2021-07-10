319b182304b2b0b209e15dd889c71923f040e41b
Jamal Bencharki <bencharki.jamal@gmail.com>
2021-07-10 18:13:56 +0200

    saved nav mesh onto disk

---
+++ 3d_prototype/game_world/world_grid.gd
@@ -0,0 +1,15 @@
+class_name WorldGrid
+extends Spatial
+
+
+export var cell_size := Vector3(2.0, 2.0, 2.0)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
