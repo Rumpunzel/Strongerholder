93fbe4770e562053e5e11a75a26d9ca69244ebfd
Jamal Bencharki <bencharki.jamal@gmail.com>
2021-07-12 19:19:18 +0200

    changed versioning to use ints

--- 3d_prototype/ui/game_version.gd
+++ 3d_prototype/ui/game_version.gd
@@ -2,5 +2,27 @@ class_name GameVersion
 extends Label
 tool
 
+
 func _ready():
-	text = "VERSION-%s" % [ ProjectSettings.get("application/config/version") ]
+	text = "version %s" % version_as_string(true)
+
+
+
+static func version_as_string(with_postfix := false) -> String:
+	var version := "%d.%d.%d"
+	
+	if with_postfix:
+		var postfix: String = ProjectSettings.get("application/config/version_postfix")
+		version += " %s" % postfix
+	
+	return version % [ major_version(), minor_version(), patch_version() ]
+
+
+static func major_version() -> int:
+	return ProjectSettings.get("application/config/version_major")
+
+static func minor_version() -> int:
+	return ProjectSettings.get("application/config/version_minor")
+
+static func patch_version() -> int:
+	return ProjectSettings.get("application/config/version_patch")
