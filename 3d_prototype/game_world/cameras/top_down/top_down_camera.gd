2a0d3c0e100fb16a1dd9fe1b586d80fd5b350e8c
Jamal Bencharki <bencharki.jamal@gmail.com>
2021-07-14 16:54:40 +0200

    small camera adjustment

--- 3d_prototype/game_world/cameras/top_down/top_down_camera.gd
+++ 3d_prototype/game_world/cameras/top_down/top_down_camera.gd
@@ -12,26 +12,26 @@ var _turn_angle: float = 0.0
 var _angle: float = 0.0
 var _turnIndex: int = 0
 
-var _zoom: float = -1.0
-var _tween: Tween
+var _zoom: float = 1.0
+#var _tween: Tween
 
 
 
-func _enter_tree() -> void:
-	# warning-ignore:return_value_discarded
-	Events.main.connect("game_continued", self, "_on_game_continued")
-	_tween = $Tween
-	_zoom = _attributes.camera_max_zoom
-
-func _exit_tree() -> void:
-	Events.main.disconnect("game_continued", self, "_on_game_continued")
+#func _enter_tree() -> void:
+#	# warning-ignore:return_value_discarded
+#	Events.main.connect("game_continued", self, "_on_game_continued")
+#	_tween = $Tween
+#	_zoom = _attributes.camera_max_zoom
+#
+#func _exit_tree() -> void:
+#	Events.main.disconnect("game_continued", self, "_on_game_continued")
 
 
 func _process(delta: float) -> void:
 	if not _attributes:
 		return
 	
-	fov = _attributes.fov + (_attributes.shoulder_fov - _attributes.fov) * ((_zoom - _attributes.camera_min_zoom) / (_attributes.camera_max_zoom - _attributes.camera_min_zoom))
+	#fov = _attributes.fov + (_attributes.shoulder_fov - _attributes.fov) * ((_zoom - _attributes.camera_min_zoom) / (_attributes.camera_max_zoom - _attributes.camera_min_zoom))
 	
 	if not follow_node:
 		return
@@ -70,15 +70,15 @@ func _frame_node(node: Spatial, _delta: float) -> void:
 	_offset.z = _attributes.distance_from_follow * sin(_angle) * zoom
 	
 	_position = node.translation + _offset
-	_position.y = _attributes.distance_off_ground * inverse_zoom
+	_position.y = _attributes.shoulder_height + _attributes.distance_off_ground * inverse_zoom
 	
 	translation = _position
 	
 	look_at(Vector3(node.translation.x, _attributes.shoulder_height, node.translation.z), Vector3.UP)
 
 
-func _on_game_continued() -> void:
-	_set_zoom(_attributes.camera_min_zoom)
+#func _on_game_continued() -> void:
+#	_set_zoom(_attributes.camera_min_zoom)
 
 
 func _set_attributes(new_attributes: TopDownCameraAttributes) -> void:
@@ -89,13 +89,13 @@ func _set_attributes(new_attributes: TopDownCameraAttributes) -> void:
 	
 	_angle = _attributes.camera_angle_offset
 
-func _set_zoom(new_zoom: float) -> void:
-	new_zoom = clamp(new_zoom, _attributes.camera_min_zoom, _attributes.camera_max_zoom)
-	
-	if _zoom >= _attributes.camera_min_zoom:
-		# warning-ignore:return_value_discarded
-		_tween.interpolate_property(self, "_zoom", null, new_zoom, _attributes.camera_scroll_time, Tween.TRANS_QUAD, Tween.EASE_OUT)
-		# warning-ignore:return_value_discarded
-		_tween.start()
-	else:
-		_zoom = new_zoom
+#func _set_zoom(new_zoom: float) -> void:
+#	new_zoom = clamp(new_zoom, _attributes.camera_min_zoom, _attributes.camera_max_zoom)
+#
+#	if _zoom >= _attributes.camera_min_zoom:
+#		# warning-ignore:return_value_discarded
+#		_tween.interpolate_property(self, "_zoom", null, new_zoom, _attributes.camera_scroll_time, Tween.TRANS_QUAD, Tween.EASE_OUT)
+#		# warning-ignore:return_value_discarded
+#		_tween.start()
+#	else:
+#		_zoom = new_zoom
