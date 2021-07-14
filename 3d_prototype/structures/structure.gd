68ed5498f62ee124005389e67da4f253cd507b87
Jamal Bencharki <bencharki.jamal@gmail.com>
2021-07-14 16:55:18 +0200

    started structure refactor

--- 3d_prototype/structures/structure.gd
+++ 3d_prototype/structures/structure.gd
@@ -2,9 +2,12 @@ class_name Structure
 extends StaticBody
 tool
 
+export(Resource) var _structure_resource
 export var _mesh_index := 0 setget _set_index
 
 
 func _set_index(new_index: int) -> void:
 	_mesh_index = new_index
-	($Mesh as ArrayMeshInstance).set_index(_mesh_index)
+#	var mesh := $Mesh as ArrayMeshInstance
+#	if mesh:
+#		mesh.set_index(_mesh_index)
--- 3d_prototype/structures/structure_resource.gd
+++ 3d_prototype/structures/structure_resource.gd
@@ -1,2 +1,8 @@
 class_name StructureResource
 extends ObjectResource
+
+export(int, 1, 10) var x_dimension := 1
+export(int, 1, 10) var y_dimension := 1
+export(int, 1, 10) var z_dimension := 1
+
+export(PackedScene) var model
