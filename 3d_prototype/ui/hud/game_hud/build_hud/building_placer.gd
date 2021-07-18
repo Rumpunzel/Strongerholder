6c2e0aedd81deac0c3223fe4f75ac8bc232cbdf7
Jamal Bencharki <bencharki.jamal@gmail.com>
2021-07-18 21:01:58 +0200

    implemented building rotating

--- 3d_prototype/ui/hud/game_hud/build_hud/building_placer.gd
+++ 3d_prototype/ui/hud/game_hud/build_hud/building_placer.gd
@@ -8,7 +8,7 @@ var current_structure: StructureResource setget _set_current_structure
 
 var _objects_area := [ ]
 var _model: Spatial = null
-
+var _rotation: int = 0
 
 onready var _building_area: Area = $BuildingArea
 onready var _collision_shape: CollisionShape = $BuildingArea/CollisionShape
@@ -31,10 +31,14 @@ func _process(_delta: float) -> void:
 	
 	var mouse_position := _raycast.get_collision_point()
 	var raster_position := Vector3(stepify(mouse_position.x, _grid_size), stepify(mouse_position.y, _grid_size), stepify(mouse_position.z, _grid_size))
+	var building_rotation := Vector3(0.0, _rotation * (PI / 2.0), 0.0)
 	
 	_building_area.translation = raster_position
+	_building_area.rotation = building_rotation
 	# warning-ignore:return_value_discarded
 	_tween.interpolate_property(_model, "translation", null, raster_position, 0.02, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	# warning-ignore:return_value_discarded
+	_tween.interpolate_property(_model, "rotation", null, building_rotation, 0.1, Tween.TRANS_QUAD, Tween.EASE_OUT)
 	# warning-ignore:return_value_discarded
 	_tween.start()
 	_model.visible = is_space_legal()
@@ -44,9 +48,14 @@ func _input(event: InputEvent) -> void:
 	if not current_structure:
 		return
 	
+	if event.is_action_pressed("rotate_building_right"):
+		_rotation -= 1
+	elif event.is_action_pressed("rotate_building_left"):
+		_rotation += 1
+	
 	if event.is_action_pressed("place_building"):
 		# warning-ignore:return_value_discarded
-		current_structure.place_at(_building_area.translation)
+		current_structure.place_at(_building_area.translation, fposmod(_rotation, 4.0) * (PI / 2.0))
 		get_tree().set_input_as_handled()
 	elif event.is_action_pressed("place_building_cancel"):
 		_set_current_structure(null)
@@ -86,6 +95,7 @@ func _set_current_structure(new_structure: StructureResource) -> void:
 		add_child(_model)
 	else:
 		_collision_shape.shape = null
+		_rotation = 0
 	
 	_collision_shape.disabled = not is_not_null
 	_raycast.enabled = is_not_null
