4b5d78f56cd97455b17fdc2f5830b960da07521a
Jamal Bencharki <bencharki.jamal@gmail.com>
2021-07-18 19:59:45 +0200

    removed hurt box form tool

--- 3d_prototype/items/equippable_item.gd
+++ 3d_prototype/items/equippable_item.gd
@@ -3,24 +3,6 @@ extends Spatial
 
 export(Resource) var item_resource
 
-onready var _hurt_shape: CollisionShape = $HurtBox/CollisionShape
-
-
-func attack(started: bool) -> void:
-	_hurt_shape.disabled = not started
-
-
-func _on_hurt_box_entered(area: Area) -> void:
-	if not area is HitBox:
-		return
-	
-	var hit_box := area as HitBox
-	for other_group in hit_box.owner.get_groups():
-		if item_resource.used_on.has(other_group):
-			# warning-ignore:return_value_discarded
-			hit_box.damage(item_resource.damage, self)
-
-
 func _get_configuration_warning() -> String:
 	var warning := ""
 	
