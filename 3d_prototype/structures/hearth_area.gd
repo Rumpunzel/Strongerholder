0bc237a8d792fc104ff99480cddfe55bb69e5031
Jamal Bencharki <bencharki.jamal@gmail.com>
2021-07-29 10:03:56 +0200

    enhanced campfire visuals and fixed campfire loading bug

--- 3d_prototype/structures/hearth_area.gd
+++ 3d_prototype/structures/hearth_area.gd
@@ -7,9 +7,6 @@ export(float, 1.0, 64.0, 1.0) var _maximum_range := 16.0
 export(float, 0.0, 16.0) var _minimum_energy := 0.0
 export(float, 0.0, 16.0) var _maximum_energy := 5.0
 
-export(int, 1, 128) var _minimum_particles := 4
-export(int, 1, 128) var _maximum_particles := 64
-
 export(Resource) var _item_to_burn
 export var _item_burn_duration := 60.0
 
@@ -24,7 +21,10 @@ var _previous_burn_stage := 0
 onready var _inventory: Inventory = Utils.find_node_of_type_in_children(owner, Inventory)
 onready var _collision_shape: CollisionShape = $CollisionShape
 onready var _light: OmniLight = $Light
-onready var _particles: Particles = $Particles
+
+onready var _embers: Particles = $Embers
+onready var _smoke: Particles = $Smoke
+onready var _fire: Particles = $Fire
 
 onready var _burn_stages: int = _item_to_burn.stack_size
 onready var _maximum_burn_duration := _burn_stages * _item_burn_duration
@@ -49,9 +49,17 @@ func _process(delta: float) -> void:
 
 func save_to_var(save_file: File) -> void:
 	save_file.store_var(_current_burn_duration)
+	save_file.store_8(_current_burn_stage)
+	save_file.store_8(_previous_burn_stage)
 
 func load_from_var(save_file: File) -> void:
 	_current_burn_duration = save_file.get_var()
+	_current_burn_stage = save_file.get_8()
+	_previous_burn_stage = save_file.get_8()
+	
+	var burning := _current_burn_duration > 0.0
+	_burn(burning, 0.0)
+	_blaze(burning)
 
 
 
@@ -66,12 +74,13 @@ func _burn(burning: bool, delta: float) -> void:
 	if _previous_burn_stage > _current_burn_stage:
 		var left_in_stack := _inventory.use(_item_to_burn)
 		assert(left_in_stack >= 0)
-		_particles.amount = int(_value_from_range(_minimum_particles, _maximum_particles))
 
 
 func _blaze(burning: bool) -> void:
 	_light.visible = burning
-	_particles.emitting = burning
+	_embers.emitting = burning
+	_smoke.emitting = _current_burn_stage > 1
+	_fire.emitting = _current_burn_stage > 2
 	
 	if not burning:
 		return
@@ -88,6 +97,10 @@ func _value_from_range(min_value: float, max_value: float) -> float:
 	return min_value + ratio * (max_value - min_value)
 
 
-func _on_item_added(item: ItemResource) -> void:
-	if item == _item_to_burn and _current_burn_duration < _maximum_burn_duration:
+func _on_item_added(_item: ItemResource) -> void:
+	if _inventory.count(_item_to_burn) > _current_burn_stage:
 		_current_burn_duration += _item_burn_duration
+
+func _on_item_removed(_item: ItemResource) -> void:
+	if _inventory.count(_item_to_burn) < _current_burn_stage:
+		_current_burn_duration -= _item_burn_duration
