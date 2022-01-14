5e1439fc57354c12f7bc3b1c9038ba1a1389c198
Jamal Bencharki <bencharki.jamal@gmail.com>
2022-01-14 16:27:59 +0100

    made event channel error into warnings

--- 3d_prototype/event_system/_event_channel_base_resource.gd
+++ 3d_prototype/event_system/_event_channel_base_resource.gd
@@ -8,5 +8,5 @@ func _has_connections(signal_name: String) -> bool:
 	if not get_signal_connection_list(signal_name).empty():
 		return true
 	else:
-		printerr("signal %s on %s not connected to anything, emission skipped" % [ signal_name, resource_path ])
+		push_warning("signal %s on %s not connected to anything, emission skipped" % [ signal_name, resource_path ])
 		return false
