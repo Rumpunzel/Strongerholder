ed329f05c04bbde80623503d44d0789d879d33de
Jamal Bencharki <bencharki.jamal@gmail.com>
2022-08-27 13:34:18 +0200

    cleaned up inspector

--- state_machine/addons/state_machine/inspector/transition_table_inspector_plugin.gd
+++ state_machine/addons/state_machine/inspector/transition_table_inspector_plugin.gd
@@ -1,7 +1,6 @@
 extends EditorInspectorPlugin
 tool
 
-const TransitionTableEditorButton := preload("res://addons/state_machine/inspector/transition_table_editor_button.gd")
 const StateMachineGraphEditScene := preload("res://addons/state_machine/inspector/state_machine_graph_edit.tscn")
 
 
@@ -9,18 +8,15 @@ func can_handle(object: Object) -> bool:
 	return object is TransitionTableResource
 
 func parse_property(object: Object, type: int, path: String, hint: int, hint_text: String, usage: int) -> bool:
-	if path == "_transitions":
-		#var button := TransitionTableEditorButton.new()
-		var graph_edit := StateMachineGraphEditScene.instance()
-		
-		#button.toggle_button.pressed = object is TransitionTableResource
-		#graph_edit.visible = object is TransitionTableResource
-		#button.connect("property_updated", graph_edit, "_on_transitions_updated" )
-		#button.toggle_button.connect("toggled", graph_edit, "set_visible")
-		#add_property_editor(path, button)
-		add_custom_control(graph_edit)
-		graph_edit.transition_table = object
-		
-		return false
-	else:
-		return false
+	match path:
+		"entry_state_resource":
+			return true
+		"_transitions":
+			var graph_edit := StateMachineGraphEditScene.instance()
+			add_custom_control(graph_edit)
+			graph_edit.transition_table = object
+			return true
+		"_graph_offsets":
+			return true
+		_:
+			return false
