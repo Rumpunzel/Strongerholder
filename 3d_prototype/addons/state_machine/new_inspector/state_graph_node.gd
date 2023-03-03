bc5f4b4a554626109dcb120c64c64a705ec13ab2
Jamal Bencharki <bencharki.jamal@gmail.com>
2023-03-03 14:34:45 +0100

    fixed layout for new node-based state machine

--- 3d_prototype/addons/state_machine/new_inspector/state_graph_node.gd
+++ 3d_prototype/addons/state_machine/new_inspector/state_graph_node.gd
@@ -1,8 +1,6 @@
 extends "res://addons/state_machine/new_inspector/graph_node.gd"
 tool
 
-signal delete_requested()
-
 var state_resource: NodePath setget set_state_resource
 var entry_status := false setget set_entry_status
 
@@ -17,6 +15,3 @@ func set_entry_status(new_entry_node: bool) -> void:
 func set_state_resource(new_state_resource: NodePath) -> void:
 	state_resource = new_state_resource
 	$Subtitle.text = state_resource
-
-func _on_deleted() -> void:
-	emit_signal("delete_requested")
